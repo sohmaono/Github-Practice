039ea7fb5fab11c7896c67740b388d5803ef11d3
sohmaono <ziphub2024@gmail.com>
2026-06-08 21:57:35 +0900

    Initial Commit

---
+++ Github Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Github Practice
+//
+//  Created by 小野聡真 on 2026/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Github Practice/Github_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_PracticeApp.swift
+//  Github Practice
+//
+//  Created by 小野聡真 on 2026/06/08.
+//
+
+import SwiftUI
+
+@main
+struct Github_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
